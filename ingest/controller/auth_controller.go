cd75946b340c08adc3aceeba6d2a5c97cebfb035
Bharat Kathi <bharat1031@gmail.com>
2024-01-16 01:31:46 -0800

    user auth login

--- ingest/controller/auth_controller.go
+++ ingest/controller/auth_controller.go
@@ -26,7 +26,19 @@ func RegisterAccount(c *gin.Context) {
 }
 
 func LoginAccount(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"message": "pong",
-	})
+	var input model.User
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+		return
+	}
+	if service.GetIDForEmail(input.Email) == "" {
+		c.JSON(http.StatusNotFound, gin.H{"message": "Account with this email does not exist"})
+		return
+	}
+	token, err := service.LoginAccount(input)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"id": service.GetIDForEmail(input.Email), "token": token})
 }
