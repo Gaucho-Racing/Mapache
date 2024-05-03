50096384fd3eb392b025db07b4688236ace9ff99
Bharat Kathi <bharat1031@gmail.com>
2024-05-02 19:53:34 -0700

    bruh i need to be freed

--- ingest/service/vehicle_service_test.go
+++ ingest/service/vehicle_service_test.go
@@ -31,8 +31,8 @@ func TestMain(m *testing.M) {
 			utils.SugarLogger.Fatalf("failed to terminate container: %s", err)
 		}
 	}()
-	name, err := mysqlContainer.Name(ctx)
-	config.DatabaseHost = name
+	//name, err := mysqlContainer.Name(ctx)
+	//config.DatabaseHost = name
 	s, err := mysqlContainer.ConnectionString(ctx)
 	println(s)
 
