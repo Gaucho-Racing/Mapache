cc0e58eb737116a30dacc1757afee3b5adf66215
Bharat Kathi <bharat1031@gmail.com>
2024-05-02 23:27:11 -0700

    fix rigby pedal test

--- rigby/tests/nodes/gr24/pedal_test.py
+++ rigby/tests/nodes/gr24/pedal_test.py
@@ -4,8 +4,8 @@ from rigby.nodes.gr24.pedal import Pedal
 def test_generate():
     pedal = Pedal()
     pedal.generate()
-    assert pedal.APPS1 >= 0 and pedal.APPS1 <= 100
-    assert pedal.APPS2 >= 0 and pedal.APPS2 <= 100
+    assert pedal.APPS1 >= 44256 and pedal.APPS1 <= 50100
+    assert pedal.APPS2 >= 38750 and pedal.APPS2 <= 41810
     assert pedal.brake_pressure_front >= 0 and pedal.brake_pressure_front <= 256
     assert pedal.brake_pressure_rear >= 0 and pedal.brake_pressure_rear <= 256
 
