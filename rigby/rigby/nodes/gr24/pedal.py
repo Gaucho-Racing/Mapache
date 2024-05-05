85b5f85028e0dc6f025180ba646866cabe528e9c
Bharat Kathi <bharat1031@gmail.com>
2024-05-05 03:24:38 -0700

    update rigby pedal weights

--- rigby/rigby/nodes/gr24/pedal.py
+++ rigby/rigby/nodes/gr24/pedal.py
@@ -23,8 +23,8 @@ class Pedal:
 
     def generate(self):
         self.millis += 1
-        self.APPS1 = Valgen.smart_rand(44256, 50100, self.APPS1, 500, 0.35)
-        self.APPS2 = Valgen.smart_rand(38750, 41810, self.APPS2, 500, 0.35)
+        self.APPS1 = Valgen.smart_rand(44256, 50100, self.APPS1, 500, 0.5)
+        self.APPS2 = Valgen.smart_rand(38750, 41810, self.APPS2, 500, 0.5)
         self.brake_pressure_front = Valgen.smart_rand(0, 256, self.brake_pressure_front, 10)
         self.brake_pressure_rear = Valgen.smart_rand(0, 256, self.brake_pressure_rear, 10)
 
