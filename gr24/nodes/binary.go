64be4d2b442b69cafefbc89b79273a90d8b126ca
Bharat Kathi <bharat1031@gmail.com>
2024-05-24 00:06:42 -0700

    holy fuck we got binary working

--- gr24/nodes/binary.go
+++ gr24/nodes/binary.go
@@ -14,10 +14,11 @@ func BigEndianUnsignedIntToBinaryString(num int, num_bytes int) (string, error) 
 		return "", fmt.Errorf("number is too large to fit in %d bytes", num_bytes)
 	}
 	bytes, _ := BigEndianUnsignedIntToBinary(num, num_bytes)
+	var bs = ""
 	for i := 0; i < num_bytes; i++ {
-		fmt.Printf("%08b", bytes[i])
+		bs += fmt.Sprintf("%08b", bytes[i])
 	}
-	return "", nil
+	return bs, nil
 }
 
 func BigEndianUnsignedIntToBinary(num int, num_bytes int) ([]byte, error) {
@@ -36,42 +37,36 @@ func BigEndianUnsignedIntToBinary(num int, num_bytes int) ([]byte, error) {
 }
 
 func TestBigEndianUnsignedIntToBinaryString() {
+	println("TestBigEndianUnsignedIntToBinaryString")
 	var bs = ""
-	// Test negative number
-	test_num := -1
-	_, err := BigEndianUnsignedIntToBinaryString(test_num, 2)
-	if err != nil {
-		println("PASS: " + err.Error())
-	}
-	// Test negative bytes
-	test_num = 0
-	_, err = BigEndianUnsignedIntToBinaryString(test_num, -2)
-	if err != nil {
-		println("PASS: " + err.Error())
-	}
-	// Test invalid bytes
-	test_num = 193852
-	_, err = BigEndianUnsignedIntToBinaryString(test_num, 2)
-	if err != nil {
-		println("PASS: " + err.Error())
-	}
 	// Test minumum number (0)
-	test_num = 0
-	bs, err = BigEndianUnsignedIntToBinaryString(test_num, 2)
+	num := 0
+	fmt.Printf("%d: ", num)
+	bs, err := BigEndianUnsignedIntToBinaryString(num, 2)
 	if err != nil {
 		log.Println(err.Error())
 	}
 	println(bs)
 	// Test max number (65535)
-	test_num = 65535
-	bs, err = BigEndianUnsignedIntToBinaryString(test_num, 2)
+	num = 65535
+	fmt.Printf("%d: ", num)
+	bs, err = BigEndianUnsignedIntToBinaryString(num, 2)
 	if err != nil {
 		log.Println(err.Error())
 	}
 	println(bs)
 	// Test 3172
-	test_num = 3172
-	bs, err = BigEndianUnsignedIntToBinaryString(test_num, 2)
+	num = 3172
+	fmt.Printf("%d: ", num)
+	bs, err = BigEndianUnsignedIntToBinaryString(num, 2)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	println(bs)
+	// Test 3172681251
+	num = 3172681251
+	fmt.Printf("%d: ", num)
+	bs, err = BigEndianUnsignedIntToBinaryString(num, 4)
 	if err != nil {
 		log.Println(err.Error())
 	}
@@ -95,10 +90,11 @@ func LittleEndianUnsignedIntToBinaryString(num int, num_bytes int) (string, erro
 		return "", fmt.Errorf("number is too large to fit in %d bytes", num_bytes)
 	}
 	bytes, _ := LittleEndianUnsignedIntToBinary(num, num_bytes)
+	var bs = ""
 	for i := 0; i < num_bytes; i++ {
-		fmt.Printf("%08b", bytes[i])
+		bs += fmt.Sprintf("%08b", bytes[i])
 	}
-	return "", nil
+	return bs, nil
 }
 
 func LittleEndianUnsignedIntToBinary(num int, num_bytes int) ([]byte, error) {
@@ -112,48 +108,41 @@ func LittleEndianUnsignedIntToBinary(num int, num_bytes int) ([]byte, error) {
 	var result []byte
 	for i := 0; i < num_bytes; i++ {
 		result = append(result, byte(num>>uint(i*8)))
-		println(result[i])
 	}
 	return result, nil
 }
 
 func TestLittleEndianUnsignedIntToBinaryString() {
+	println("TestLittleEndianUnsignedIntToBinaryString")
 	var bs = ""
-	// Test negative number
-	test_num := -1
-	_, err := LittleEndianUnsignedIntToBinaryString(test_num, 2)
-	if err != nil {
-		println("PASS: " + err.Error())
-	}
-	// Test negative bytes
-	test_num = 0
-	_, err = LittleEndianUnsignedIntToBinaryString(test_num, -2)
-	if err != nil {
-		println("PASS: " + err.Error())
-	}
-	// Test invalid bytes
-	test_num = 193852
-	_, err = LittleEndianUnsignedIntToBinaryString(test_num, 2)
-	if err != nil {
-		println("PASS: " + err.Error())
-	}
 	// Test minumum number (0)
-	test_num = 0
-	bs, err = LittleEndianUnsignedIntToBinaryString(test_num, 2)
+	num := 0
+	fmt.Printf("%d: ", num)
+	bs, err := LittleEndianUnsignedIntToBinaryString(num, 2)
 	if err != nil {
 		log.Println(err.Error())
 	}
 	println(bs)
 	// Test max number (65535)
-	test_num = 65535
-	bs, err = LittleEndianUnsignedIntToBinaryString(test_num, 2)
+	num = 65535
+	fmt.Printf("%d: ", num)
+	bs, err = LittleEndianUnsignedIntToBinaryString(num, 2)
 	if err != nil {
 		log.Println(err.Error())
 	}
 	println(bs)
 	// Test 3172
-	test_num = 3172
-	bs, err = LittleEndianUnsignedIntToBinaryString(test_num, 2)
+	num = 3172
+	fmt.Printf("%d: ", num)
+	bs, err = LittleEndianUnsignedIntToBinaryString(num, 2)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	println(bs)
+	// Test 3172681251
+	num = 3172681251
+	fmt.Printf("%d: ", num)
+	bs, err = LittleEndianUnsignedIntToBinaryString(num, 4)
 	if err != nil {
 		log.Println(err.Error())
 	}
@@ -200,9 +189,206 @@ func TestBigEndianBytesToUnsignedInt() {
 	println(BigEndianBytesToUnsignedInt(bytes))
 }
 
+func BigEndianSignedIntToBinaryString(num int, num_bytes int) (string, error) {
+	if num_bytes < 1 {
+		return "", fmt.Errorf("cannot convert to binary with less than 1 byte")
+	}
+	minValue := -1 << ((num_bytes * 8) - 1)
+	maxValue := (1 << ((num_bytes * 8) - 1)) - 1
+	if num < minValue || num > maxValue {
+		return "", fmt.Errorf("number is too large to fit in %d bytes", num_bytes)
+	}
+	bytes, _ := BigEndianSignedIntToBinary(num, num_bytes)
+	var bs = ""
+	println()
+	for i := 0; i < num_bytes; i++ {
+		bs += fmt.Sprintf("%08b", bytes[i])
+		fmt.Printf("%02x ", bytes[i])
+	}
+	println()
+	return bs, nil
+}
+
+func BigEndianSignedIntToBinary(num int, num_bytes int) ([]byte, error) {
+	if num_bytes < 1 {
+		return nil, fmt.Errorf("cannot convert to binary with less than 1 byte")
+	}
+	minValue := -1 << ((num_bytes * 8) - 1)
+	maxValue := (1 << ((num_bytes * 8) - 1)) - 1
+	if num < minValue || num > maxValue {
+		return nil, fmt.Errorf("number is too large to fit in %d bytes", num_bytes)
+	}
+	var result []byte
+	for i := 0; i < num_bytes; i++ {
+		shift := uint((num_bytes - i - 1) * 8)
+		result = append(result, byte(num>>shift))
+	}
+	return result, nil
+}
+
+func TestBigEndianSignedIntToBinaryString() {
+	println("TestBigEndianSignedIntToBinaryString")
+	// Test 0
+	var num = 0
+	fmt.Printf("%d: ", num)
+	bs, err := BigEndianSignedIntToBinaryString(num, 2)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	println(bs)
+	// Test 65535
+	num = -65535
+	fmt.Printf("%d: ", num)
+	bs, err = BigEndianSignedIntToBinaryString(num, 2)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	println(bs)
+	// Test 3172
+	num = -3172
+	fmt.Printf("%d: ", num)
+	bs, err = BigEndianSignedIntToBinaryString(num, 2)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	println(bs)
+	num = -1934
+	fmt.Printf("%d: ", num)
+	bs, err = BigEndianSignedIntToBinaryString(num, 2)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	println(bs)
+}
+
+func LittleEndianSignedIntToBinaryString(num int, num_bytes int) (string, error) {
+	if num_bytes < 1 {
+		return "", fmt.Errorf("cannot convert to binary with less than 1 byte")
+	}
+	minValue := -1 << ((num_bytes * 8) - 1)
+	maxValue := (1 << ((num_bytes * 8) - 1)) - 1
+	if num < minValue || num > maxValue {
+		return "", fmt.Errorf("number is too large to fit in %d bytes", num_bytes)
+	}
+	bytes, _ := LittleEndianSignedIntToBinary(num, num_bytes)
+	var bs = ""
+	println()
+	for i := 0; i < num_bytes; i++ {
+		bs += fmt.Sprintf("%08b", bytes[i])
+		fmt.Printf("%02x ", bytes[i])
+	}
+	println()
+	return bs, nil
+}
+
+func LittleEndianSignedIntToBinary(num int, num_bytes int) ([]byte, error) {
+	if num_bytes < 1 {
+		return nil, fmt.Errorf("cannot convert to binary with less than 1 byte")
+	}
+	minValue := -1 << ((num_bytes * 8) - 1)
+	maxValue := (1 << ((num_bytes * 8) - 1)) - 1
+	if num < minValue || num > maxValue {
+		return nil, fmt.Errorf("number is too large to fit in %d bytes", num_bytes)
+	}
+	var result []byte
+	for i := 0; i < num_bytes; i++ {
+		shift := uint(i * 8)
+		result = append(result, byte(num>>shift))
+	}
+	return result, nil
+}
+
+func TestLittleEndianSignedIntToBinaryString() {
+	println("TestLittleEndianSignedIntToBinaryString")
+	// Test 0
+	var num = 0
+	fmt.Printf("%d: ", num)
+	bs, err := LittleEndianSignedIntToBinaryString(num, 2)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	println(bs)
+	// Test 65535
+	num = -65535
+	fmt.Printf("%d: ", num)
+	bs, err = LittleEndianSignedIntToBinaryString(num, 2)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	println(bs)
+	// Test 3172
+	num = -3172
+	fmt.Printf("%d: ", num)
+	bs, err = LittleEndianSignedIntToBinaryString(num, 2)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	println(bs)
+	num = -1934
+	fmt.Printf("%d: ", num)
+	bs, err = LittleEndianSignedIntToBinaryString(num, 2)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	println(bs)
+}
+
+type SignMode int
+
+const (
+	Signed   SignMode = 1
+	Unsigned SignMode = 0
+)
+
+type Endian int
+
+const (
+	BigEndian    Endian = 1
+	LittleEndian Endian = 0
+)
+
+type Field struct {
+	Name   string
+	Bytes  []byte
+	Value  int
+	Length int
+	Sign   SignMode
+	Endian Endian
+}
+
+func (f Field) Decode() int {
+	if f.Sign == Signed && f.Endian == BigEndian {
+		// TODO
+	} else if f.Sign == Signed && f.Endian == LittleEndian {
+		// TODO
+	} else if f.Sign == Unsigned && f.Endian == BigEndian {
+		return BigEndianBytesToUnsignedInt(f.Bytes)
+	} else if f.Sign == Unsigned && f.Endian == LittleEndian {
+		return LittleEndianBytesToUnsignedInt(f.Bytes)
+	}
+	return 0
+}
+
+func (f Field) Encode() ([]byte, error) {
+	if f.Sign == Signed && f.Endian == BigEndian {
+		return BigEndianSignedIntToBinary(f.Value, f.Length)
+	} else if f.Sign == Signed && f.Endian == LittleEndian {
+		return LittleEndianSignedIntToBinary(f.Value, f.Length)
+	} else if f.Sign == Unsigned && f.Endian == BigEndian {
+		return BigEndianUnsignedIntToBinary(f.Value, f.Length)
+	} else if f.Sign == Unsigned && f.Endian == LittleEndian {
+		return LittleEndianUnsignedIntToBinary(f.Value, f.Length)
+	}
+	return nil, fmt.Errorf("invalid sign or endian")
+}
+
 func main() {
+
 	TestBigEndianUnsignedIntToBinaryString()
 	TestLittleEndianUnsignedIntToBinaryString()
-	TestLittleEndianBytesToUnsignedInt()
-	TestBigEndianBytesToUnsignedInt()
+	// TestLittleEndianBytesToUnsignedInt()
+	// TestBigEndianBytesToUnsignedInt()
+	println("-------------")
+	TestBigEndianSignedIntToBinaryString()
+	TestLittleEndianSignedIntToBinaryString()
 }
