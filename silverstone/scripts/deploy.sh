3d715748e352f62c5ef7f6e3e648e0e79e7fc008
Bharat Kathi <bharat1031@gmail.com>
2024-05-27 19:19:21 -0700

    add deploy scripts

---
+++ silverstone/scripts/deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+service_name=$(basename $(dirname $(dirname "$(readlink -f "$0")")))
+
+# Check if version is provided
+if [ -z "$1" ]
+  then
+    echo "No version number provided"
+    exit 1
+fi
+
+# Check if docker is installed
+if ! [ -x "$(command -v docker)" ]; then
+  echo 'Error: docker is not installed.' >&2
+  exit 1
+fi
+
+echo "Building container for $service_name v$1"
+# Build the docker container
+docker build -t gauchoracing/$service_name:"$1" -t gauchoracing/$service_name:latest --platform linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6 --push --progress=plain .
