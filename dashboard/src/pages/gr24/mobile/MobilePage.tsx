ceadbd7d22798771ab9bf376e4008c3429870955
Bharat Kathi <bharat1031@gmail.com>
2024-05-31 03:44:08 -0700

    added debug fields to mobile node

--- dashboard/src/pages/gr24/mobile/MobilePage.tsx
+++ dashboard/src/pages/gr24/mobile/MobilePage.tsx
@@ -40,6 +40,9 @@ function MobilePage() {
             speed: 4,
             bearing: data.heading,
           });
+          const marker = new mapboxgl.Marker()
+            .setLngLat([data.longitude, data.latitude])
+            .addTo(map.current);
         }
       }
     }
@@ -49,6 +52,38 @@ function MobilePage() {
     const compass = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"];
     const index = Math.round(messageJson.heading / 45) % 8;
     return compass[index];
+  };
+
+  const CompassDisplay = () => {
+    return (
+      <div
+        className="rounded-md bg-card p-4 text-center opacity-80"
+        style={{ width: "130px" }}
+      >
+        <h4>
+          {messageJson.heading.toFixed(1)}° {getCompassString()}
+        </h4>
+      </div>
+    );
+  };
+
+  const SpeedDisplay = () => {
+    return (
+      <div className="flex items-center rounded-md bg-card p-4 text-center opacity-80">
+        <div style={{ width: "50px" }}>
+          <h1>{(messageJson.speed * 2.23694).toFixed(0)}</h1>
+          <div>MPH</div>
+        </div>
+        <div
+          className="mx-2 border-l border-gray-400"
+          style={{ height: "50px" }}
+        ></div>
+        <div style={{ width: "50px" }}>
+          <h1>{(messageJson.speed * 3.6).toFixed(0)}</h1>
+          <div>KMPH</div>
+        </div>
+      </div>
+    );
   };
 
   return (
@@ -64,36 +99,80 @@ function MobilePage() {
           <div
             ref={mapContainer}
             className="map-container absolute left-0 top-0 w-full rounded-lg"
-            style={{ height: "calc(50vh - 2rem)" }} // Adjust height to account for the speed display
+            style={{ height: "50vh" }}
           />
-          <div
-            className="absolute rounded-md bg-card p-4 text-center opacity-80"
-            style={{ top: "12px", left: "12px", width: "130px" }}
-          >
-            <h4>
-              {messageJson.heading.toFixed(1)}° {getCompassString()}
-            </h4>
+          <div className="absolute" style={{ top: "12px", left: "12px" }}>
+            <CompassDisplay />
           </div>
-          <div
-            className="absolute flex items-center rounded-md bg-card p-4 text-center opacity-80"
-            style={{ bottom: "42px", right: "12px" }}
-          >
-            <div style={{ width: "50px" }}>
-              <h1>{(messageJson.speed * 2.23694).toFixed(0)}</h1>
-              <div>MPH</div>
-            </div>
-            <div
-              className="mx-2 border-l border-gray-400"
-              style={{ height: "50px" }}
-            ></div>
-            <div style={{ width: "50px" }}>
-              <h1>{(messageJson.speed * 3.6).toFixed(0)}</h1>
-              <div>KMPH</div>
-            </div>
+          <div className="absolute" style={{ bottom: "12px", right: "12px" }}>
+            <SpeedDisplay />
           </div>
         </div>
-        <div className="rounded-md bg-card p-4 text-center opacity-80">
-          Speed: {messageJson.speed.toFixed(1)} m/s
+        <div className="m-4 flex w-full flex-row text-lg">
+          <div className="flex-1">
+            <div className="flex">
+              <div className="pr-2 font-semibold">Latitude:</div>
+              <div className="font-semibold text-gray-400">
+                {messageJson.latitude.toFixed(6)}
+              </div>
+            </div>
+            <div className="mt-1 flex">
+              <div className="pr-2 font-semibold">Longitude:</div>
+              <div className="font-semibold text-gray-400">
+                {messageJson.longitude.toFixed(6)}
+              </div>
+            </div>
+            <div className="mt-1 flex">
+              <div className="pr-2 font-semibold">Altitude:</div>
+              <div className="font-semibold text-gray-400">
+                {messageJson.altitude.toFixed(6)} m
+              </div>
+            </div>
+            <div className="mt-1 flex">
+              <div className="pr-2 font-semibold">Heading:</div>
+              <div className="font-semibold text-gray-400">
+                {messageJson.heading.toFixed(6)} °
+              </div>
+            </div>
+            <div className="mt-1 flex">
+              <div className="pr-2 font-semibold">Speed:</div>
+              <div className="font-semibold text-gray-400">
+                {messageJson.speed} m/s
+              </div>
+            </div>
+            <div className="mt-1 flex">
+              <div className="pr-2 font-semibold">Last Update:</div>
+              <div className="font-semibold text-gray-400">
+                {new Date(messageJson.created_at).toISOString()}
+              </div>
+            </div>
+          </div>
+          <div className="flex-1">
+            <div className="flex">
+              <div className="pr-2 font-semibold">Accelerometer:</div>
+              <div className="font-semibold text-gray-400">
+                <div>x: {messageJson.accelerometer_x.toFixed(8)}</div>
+                <div>y: {messageJson.accelerometer_y.toFixed(8)}</div>
+                <div>z: {messageJson.accelerometer_z.toFixed(8)}</div>
+              </div>
+            </div>
+            <div className="flex">
+              <div className="pr-2 font-semibold">Gyroscope:</div>
+              <div className="font-semibold text-gray-400">
+                <div>x: {messageJson.gyroscope_x.toFixed(8)}</div>
+                <div>y: {messageJson.gyroscope_y.toFixed(8)}</div>
+                <div>z: {messageJson.gyroscope_z.toFixed(8)}</div>
+              </div>
+            </div>
+            <div className="flex">
+              <div className="pr-2 font-semibold">Magnetometer:</div>
+              <div className="font-semibold text-gray-400">
+                <div>x: {messageJson.magnetometer_x.toFixed(8)}</div>
+                <div>y: {messageJson.magnetometer_y.toFixed(8)}</div>
+                <div>z: {messageJson.magnetometer_z.toFixed(8)}</div>
+              </div>
+            </div>
+          </div>
         </div>
       </Layout>
     </>
