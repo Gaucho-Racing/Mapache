d2fc41458236de84256208545045ed7c70042537
Bharat Kathi <bharat1031@gmail.com>
2024-06-02 18:59:45 -0700

    made live graphs not have a seizure

--- dashboard/src/pages/gr24/mobile/widgets/AltitudeGraphLiveWidget.tsx
+++ dashboard/src/pages/gr24/mobile/widgets/AltitudeGraphLiveWidget.tsx
@@ -11,7 +11,6 @@ import {
   YAxis,
   CartesianGrid,
   Tooltip,
-  Legend,
   ResponsiveContainer,
 } from "recharts";
 import { Card } from "@/components/ui/card";
@@ -58,51 +57,6 @@ function AltitudeGraphLiveWidget() {
       );
     }
   };
-
-  const testData = [
-    {
-      name: "Page A",
-      uv: 4000,
-      pv: 2400,
-      amt: 2400,
-    },
-    {
-      name: "Page B",
-      uv: 3000,
-      pv: 1398,
-      amt: 2210,
-    },
-    {
-      name: "Page C",
-      uv: 2000,
-      pv: 9800,
-      amt: 2290,
-    },
-    {
-      name: "Page D",
-      uv: 2780,
-      pv: 3908,
-      amt: 2000,
-    },
-    {
-      name: "Page E",
-      uv: 1890,
-      pv: 4800,
-      amt: 2181,
-    },
-    {
-      name: "Page F",
-      uv: 2390,
-      pv: 3800,
-      amt: 2500,
-    },
-    {
-      name: "Page G",
-      uv: 3490,
-      pv: 4300,
-      amt: 2100,
-    },
-  ];
 
   const CustomTooltip = ({ active, payload, label }) => {
     if (active && payload && payload.length) {
@@ -158,6 +112,7 @@ function AltitudeGraphLiveWidget() {
                   dataKey="heading"
                   stroke="#e105a3"
                   strokeWidth={2}
+                  animateNewValues={false}
                 />
                 {/* <Line type="monotone" dataKey="altitude" stroke="#82ca9d" /> */}
               </LineChart>
