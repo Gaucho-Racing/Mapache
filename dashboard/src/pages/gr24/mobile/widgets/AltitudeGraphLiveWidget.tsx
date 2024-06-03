043bd708e7a8a445c18ddbff942100be3870e5d1
Bharat Kathi <bharat1031@gmail.com>
2024-06-02 17:32:47 -0700

    shit gettin tragic

--- dashboard/src/pages/gr24/mobile/widgets/AltitudeGraphLiveWidget.tsx
+++ dashboard/src/pages/gr24/mobile/widgets/AltitudeGraphLiveWidget.tsx
@@ -4,16 +4,31 @@ import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleXmark } from "@fortawesome/free-regular-svg-icons";
 import { Mobile, initMobile } from "@/models/gr24/mobile";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 
 function AltitudeGraphLiveWidget() {
   const [socketUrl] = React.useState("ws://localhost:10310/ws/gr24/mobile");
   const { lastMessage, readyState } = useWebSocket(socketUrl);
   const [messageJson, setMessageJson] = useState<Mobile>(initMobile);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     if (lastMessage !== null) {
       const data = JSON.parse(lastMessage.data);
       setMessageJson(data);
+      const value = Math.round(data.heading);
+      const timestamp = new Date(data.created_at);
+      const newDataPoint = [timestamp, value];
+      setData((prevData) => [...prevData.slice(-9), data]);
     }
   }, [lastMessage]);
 
@@ -44,10 +59,86 @@ function AltitudeGraphLiveWidget() {
     }
   };
 
+  const testData = [
+    {
+      name: "Page A",
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: "Page B",
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: "Page C",
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: "Page D",
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: "Page E",
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: "Page F",
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: "Page G",
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
+    },
+  ];
+
   return (
     <>
       <div className="h-full w-full">
-        {lastMessage ? <div></div> : <LoadingComponent />}
+        {lastMessage ? (
+          <div>
+            <ResponsiveContainer width="100%" height={300}>
+              <LineChart
+                width={500}
+                height={300}
+                data={data}
+                margin={{
+                  top: 5,
+                  right: 30,
+                  left: 20,
+                  bottom: 5,
+                }}
+              >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Line
+                  type="monotone"
+                  dataKey="pv"
+                  stroke="#8884d8"
+                  activeDot={{ r: 8 }}
+                />
+                <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+              </LineChart>
+            </ResponsiveContainer>
+          </div>
+        ) : (
+          <LoadingComponent />
+        )}
       </div>
     </>
   );
