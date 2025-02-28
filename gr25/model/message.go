ce22aa433d49a7298d8611fa2513c702eadf2ee4
Samprita C <sampritac9@gmail.com>
2025-02-27 19:11:21 -0800

    first

--- gr25/model/message.go
+++ gr25/model/message.go
@@ -6,6 +6,14 @@ var messageMap = map[int]mp.Message{
 	0x003: ECUStatusOne,
 	0x004: ECUStatusTwo,
 	0x005: ECUStatusThree,
+	0x007: ACUStatusOne,
+	0x008: ACUStatusTwo,
+	0x009: ACUStatusThree,
+	0x00A: ACUPrecharge,
+	0x00B: ACUConfigChargeParameters,
+	0x00C: ACUConfigOperationalParameters,
+	0x00D: ACUCellDataOne,
+	0x00E: ACUCellDataTwo,
 }
 
 func GetMessage(id int) mp.Message {
