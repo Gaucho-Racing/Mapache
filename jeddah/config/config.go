f53c696606009aa383b2b728072144dd7d119d00
Bharat Kathi <bharat1031@gmail.com>
2025-05-06 16:39:51 -0700

    fix rincon registration for jedah

--- jeddah/config/config.go
+++ jeddah/config/config.go
@@ -1,21 +1,19 @@
 package config
 
 import (
-	"fmt"
 	"os"
-	"strings"
 
 	"github.com/bk1031/rincon-go/v2"
 )
 
 var Service rincon.Service = rincon.Service{
 	Name:    "Jeddah",
-	Version: "2.1.0",
+	Version: "2.1.1",
 }
 
 var Routes = []rincon.Route{
 	{
-		Route:  fmt.Sprintf("/%s/ping", strings.ToLower(Service.Name)),
+		Route:  "/jeddah/ping",
 		Method: "*",
 	},
 	{
@@ -39,6 +37,9 @@ var Routes = []rincon.Route{
 var Env = os.Getenv("ENV")
 var Port = os.Getenv("PORT")
 
+var ServiceEndpoint = os.Getenv("SERVICE_ENDPOINT")
+var ServiceHealthCheck = os.Getenv("SERVICE_HEALTH_CHECK")
+
 var DatabaseHost = os.Getenv("DATABASE_HOST")
 var DatabasePort = os.Getenv("DATABASE_PORT")
 var DatabaseUser = os.Getenv("DATABASE_USER")
@@ -48,3 +49,4 @@ var DatabaseName = os.Getenv("DATABASE_NAME")
 var RinconClient *rincon.Client
 var RinconUser = os.Getenv("RINCON_USER")
 var RinconPassword = os.Getenv("RINCON_PASSWORD")
+var RinconEndpoint = os.Getenv("RINCON_ENDPOINT")
