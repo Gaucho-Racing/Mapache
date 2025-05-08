c23d7a56a5fb9f5a05f874e29eec33d755861a8e
Bharat Kathi <bharat1031@gmail.com>
2025-05-07 23:04:39 -0700

    jeddah: fix cors

--- jeddah/api/api.go
+++ jeddah/api/api.go
@@ -3,7 +3,11 @@ package api
 import (
 	"fmt"
 	"jeddah/config"
+	"jeddah/utils"
+	"net/http"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,6 +16,14 @@ func SetupRouter() *gin.Engine {
 		gin.SetMode(gin.ReleaseMode)
 	}
 	r := gin.Default()
+	r.Use(cors.New(cors.Config{
+		AllowAllOrigins:  true,
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization"},
+		MaxAge:           12 * time.Hour,
+		AllowCredentials: true,
+	}))
+	r.Use(UnauthorizedPanicHandler())
 	return r
 }
 
@@ -31,4 +43,80 @@ func InitializeRoutes(router *gin.Engine) {
 	router.DELETE("/trips/:tripID", DeleteTrip)
 	router.POST("/trips/:tripID/laps", CreateLap)
 	router.DELETE("/trips/:tripID/laps/:lapID", DeleteLap)
+}
+
+func UnauthorizedPanicHandler() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		defer func() {
+			if err := recover(); err != nil {
+				if err == "Unauthorized" {
+					c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "you are not authorized to access this resource"})
+				} else {
+					// Handle other panics
+					utils.SugarLogger.Errorf("Unexpected panic: %v", err)
+					c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": err.(string)})
+				}
+			}
+		}()
+		c.Next()
+	}
+}
+
+// Require checks if a condition is true, otherwise aborts the request
+func Require(c *gin.Context, condition bool) {
+	if !condition {
+		panic("Unauthorized")
+	}
+}
+
+// Any checks if any condition is true, otherwise returns false
+func Any(conditions ...bool) bool {
+	for _, condition := range conditions {
+		if condition {
+			return true
+		}
+	}
+	return false
+}
+
+// All checks if all conditions are true, otherwise returns false
+func All(conditions ...bool) bool {
+	for _, condition := range conditions {
+		if !condition {
+			return false
+		}
+	}
+	return true
+}
+
+func RequestUserHasID(c *gin.Context, id string) bool {
+	return GetRequestUserID(c) == id
+}
+
+func RequestUserHasEmail(c *gin.Context, email string) bool {
+	return GetRequestUserEmail(c) == email
+}
+
+func RequestUserHasRole(c *gin.Context, role string) bool {
+	user, err := service.GetUser(GetRequestUserID(c))
+	if err != nil {
+		return false
+	}
+	return user.HasRole(role)
+}
+
+func GetRequestUserID(c *gin.Context) string {
+	id, exists := c.Get("Auth-UserID")
+	if !exists {
+		return ""
+	}
+	return id.(string)
+}
+
+func GetRequestUserEmail(c *gin.Context) string {
+	email, exists := c.Get("Auth-Email")
+	if !exists {
+		return ""
+	}
+	return email.(string)
 }
