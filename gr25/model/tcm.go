6f5f913d8e131d7eeb17d6fad4d31ade79d8f928
Bharat Kathi <bharat1031@gmail.com>
2025-06-05 15:07:28 -0700

    gr25: rename tcm signals

--- gr25/model/tcm.go
+++ gr25/model/tcm.go
@@ -3,29 +3,29 @@ package model
 import mp "github.com/gaucho-racing/mapache-go"
 
 var TCMResourceUtil = mp.Message{
-	mp.NewField("CPU_util", 1, mp.Unsigned, mp.LittleEndian, func(f mp.Field) []mp.Signal {
+	mp.NewField("cpu_util", 1, mp.Unsigned, mp.LittleEndian, func(f mp.Field) []mp.Signal {
 		signals := []mp.Signal{}
 		signals = append(signals, mp.Signal{
-			Name:     "CPU_util",
-			Value:    float64(f.Value), //Percentage is unchanged
+			Name:     "cpu_util",
+			Value:    float64(f.Value), // Percentage is unchanged
 			RawValue: f.Value,
 		})
 		return signals
 	}),
-	mp.NewField("GPU_util", 1, mp.Unsigned, mp.LittleEndian, func(f mp.Field) []mp.Signal {
+	mp.NewField("gpu_util", 1, mp.Unsigned, mp.LittleEndian, func(f mp.Field) []mp.Signal {
 		signals := []mp.Signal{}
 		signals = append(signals, mp.Signal{
-			Name:     "GPU_util",
-			Value:    float64(f.Value), //Percentage is unchanged
+			Name:     "gpu_util",
+			Value:    float64(f.Value), // Percentage is unchanged
 			RawValue: f.Value,
 		})
 		return signals
 	}),
-	mp.NewField("RAM_util", 1, mp.Unsigned, mp.LittleEndian, func(f mp.Field) []mp.Signal {
+	mp.NewField("ram_util", 1, mp.Unsigned, mp.LittleEndian, func(f mp.Field) []mp.Signal {
 		signals := []mp.Signal{}
 		signals = append(signals, mp.Signal{
-			Name:     "RAM_util",
-			Value:    float64(f.Value), //Percentage is unchanged
+			Name:     "ram_util",
+			Value:    float64(f.Value), // Percentage is unchanged
 			RawValue: f.Value,
 		})
 		return signals
@@ -34,34 +34,34 @@ var TCMResourceUtil = mp.Message{
 		signals := []mp.Signal{}
 		signals = append(signals, mp.Signal{
 			Name:     "disk_util",
-			Value:    float64(f.Value), //Percentage is unchanged
+			Value:    float64(f.Value), // Percentage is unchanged
 			RawValue: f.Value,
 		})
 		return signals
 	}),
-	mp.NewField("power_usage", 1, mp.Unsigned, mp.LittleEndian, func(f mp.Field) []mp.Signal {
+	mp.NewField("power_util", 1, mp.Unsigned, mp.LittleEndian, func(f mp.Field) []mp.Signal {
 		signals := []mp.Signal{}
 		signals = append(signals, mp.Signal{
-			Name:     "power_usage",
-			Value:    float64(f.Value) * 0.1, //deciWatts converted to Watts
+			Name:     "power_util",
+			Value:    float64(f.Value) * 0.1, // deciWatts converted to Watts
 			RawValue: f.Value,
 		})
 		return signals
 	}),
-	mp.NewField("CPU_temp", 1, mp.Unsigned, mp.LittleEndian, func(f mp.Field) []mp.Signal {
+	mp.NewField("cpu_temp", 1, mp.Unsigned, mp.LittleEndian, func(f mp.Field) []mp.Signal {
 		signals := []mp.Signal{}
 		signals = append(signals, mp.Signal{
-			Name:     "CPU_temp",
-			Value:    float64(f.Value), //Stays as Celsius
+			Name:     "cpu_temp",
+			Value:    float64(f.Value), // stays as Celsius
 			RawValue: f.Value,
 		})
 		return signals
 	}),
-	mp.NewField("GPU_temp", 1, mp.Unsigned, mp.LittleEndian, func(f mp.Field) []mp.Signal {
+	mp.NewField("gpu_temp", 1, mp.Unsigned, mp.LittleEndian, func(f mp.Field) []mp.Signal {
 		signals := []mp.Signal{}
 		signals = append(signals, mp.Signal{
-			Name:     "GPU_temp",
-			Value:    float64(f.Value), //Stays as Celsius
+			Name:     "gpu_temp",
+			Value:    float64(f.Value), // stays as Celsius
 			RawValue: f.Value,
 		})
 		return signals
