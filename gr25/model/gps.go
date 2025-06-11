29d1f9f3de78795f4a5a3b495b94c4ea5ac06d41
Andrey <andreyotvagingrey@gmail.com>
2025-06-10 22:00:10 -0700

    fixed type casting

--- gr25/model/gps.go
+++ gr25/model/gps.go
@@ -41,122 +41,120 @@ var GPSAltitude = mp.Message{
 }
 
 var GPSPx = mp.Message{
-	mp.NewField("X_Theta", 2, mp.Unsigned, mp.LittleEndian, func(f mp.Field) []mp.Signal {
+	mp.NewField("X_Theta", 2, mp.Signed, mp.LittleEndian, func(f mp.Field) []mp.Signal {
 		signals := []mp.Signal{}
 		signals = append(signals, mp.Signal{
-			Name: "x_theta", 
-			Value: int16(f.Value) * 0.001,
-			RawValue: int16(f.Value),
+			Name:     "x_theta",
+			Value:    (float64(f.Value) * 0.01),
+			RawValue: f.Value,
 		})
 		return signals
-	}), 
-	mp.NewField("X_Acc", 2, mp.Unsigned, mp.LittleEndian, func(f mp.Field) []mp.Signal {
+	}),
+	mp.NewField("X_Acc", 2, mp.Signed, mp.LittleEndian, func(f mp.Field) []mp.Signal {
 		signals := []mp.Signal{}
 		signals = append(signals, mp.Signal{
-			Name: "x_acc", 
-			Value: int16(f.Value) * 0.01,
-			RawValue: int16(f.Value),
+			Name:     "x_acc",
+			Value:    (float64(f.Value) * 0.01),
+			RawValue: f.Value,
 		})
 		return signals
 	}),
 	mp.NewField("Status", 4, mp.Unsigned, mp.LittleEndian, func(f mp.Field) []mp.Signal {
 		signals := []mp.Signal{}
 		signals = append(signals, mp.Signal{
-			Name: "status", 
-			Value: int32(f.Value),
-			RawValue: int32(f.Value),
+			Name:     "status",
+			Value:    float64(f.Value),
+			RawValue: f.Value,
 		})
 		return signals
 	}),
-
 }
 
 var GPSQy = mp.Message{
-	mp.NewField("Y_Theta", 2, mp.Unsigned, mp.LittleEndian, func(f mp.Field) []mp.Signal {
+	mp.NewField("Y_Theta", 2, mp.Signed, mp.LittleEndian, func(f mp.Field) []mp.Signal {
 		signals := []mp.Signal{}
 		signals = append(signals, mp.Signal{
-			Name: "y_theta", 
-			Value: int16(f.Value) * 0.001,
-			RawValue: int16(f.Value),
+			Name:     "y_theta",
+			Value:    float64(f.Value) * 0.001,
+			RawValue: f.Value,
 		})
 		return signals
-	}), 
-	mp.NewField("Y_Acc", 2, mp.Unsigned, mp.LittleEndian, func(f mp.Field) []mp.Signal {
+	}),
+	mp.NewField("Y_Acc", 2, mp.Signed, mp.LittleEndian, func(f mp.Field) []mp.Signal {
 		signals := []mp.Signal{}
 		signals = append(signals, mp.Signal{
-			Name: "y_acc", 
-			Value: int16(f.Value) * 0.01,
-			RawValue: int16(f.Value),
+			Name:     "y_acc",
+			Value:    float64(f.Value) * 0.01,
+			RawValue: f.Value,
 		})
 		return signals
 	}),
 	mp.NewField("Status", 4, mp.Unsigned, mp.LittleEndian, func(f mp.Field) []mp.Signal {
 		signals := []mp.Signal{}
 		signals = append(signals, mp.Signal{
-			Name: "status", 
-			Value: int32(f.Value),
-			RawValue: int32(f.Value),
+			Name:     "status",
+			Value:    float64(f.Value),
+			RawValue: f.Value,
 		})
 		return signals
 	}),
-
 }
 
 var GPSRz = mp.Message{
-	mp.NewField("Z_Theta", 2, mp.Unsigned, mp.LittleEndian, func(f mp.Field) []mp.Signal {
+	mp.NewField("Z_Theta", 2, mp.Signed, mp.LittleEndian, func(f mp.Field) []mp.Signal {
 		signals := []mp.Signal{}
 		signals = append(signals, mp.Signal{
-			Name: "z_theta", 
-			Value: int16(f.Value) * 0.001,
-			RawValue: int16(f.Value),
+			Name:     "z_theta",
+			Value:    float64(f.Value) * 0.001,
+			RawValue: f.Value,
 		})
 		return signals
-	}), 
-	mp.NewField("Z_Acc", 2, mp.Unsigned, mp.LittleEndian, func(f mp.Field) []mp.Signal {
+	}),
+	mp.NewField("Z_Acc", 2, mp.Signed, mp.LittleEndian, func(f mp.Field) []mp.Signal {
 		signals := []mp.Signal{}
 		signals = append(signals, mp.Signal{
-			Name: "z_acc", 
-			Value: int16(f.Value) * 0.01,
-			RawValue: int16(f.Value),
+			Name:     "z_acc",
+			Value:    float64(f.Value) * 0.01,
+			RawValue: f.Value,
 		})
 		return signals
 	}),
 	mp.NewField("Status", 4, mp.Unsigned, mp.LittleEndian, func(f mp.Field) []mp.Signal {
 		signals := []mp.Signal{}
 		signals = append(signals, mp.Signal{
-			Name: "status", 
-			Value: int32(f.Value),
-			RawValue: int32(f.Value),
+			Name:     "status",
+			Value:    float64(f.Value),
+			RawValue: f.Value,
 		})
 		return signals
 	}),
 }
 
 var DGPS_UVW = mp.Message{
-	mp.NewField("DGPS_u", 2, mp.Unsigned, mp.LittleEndian, func(f mp.Field) []mp.Signal {
+	mp.NewField("DGPS_u", 2, mp.Signed, mp.LittleEndian, func(f mp.Field) []mp.Signal {
 		signals := []mp.Signal{}
 		signals = append(signals, mp.Signal{
-			Name: "dgps_u", 
-			Value: int16(f.Value) * 0.01,
-			RawValue: int16(f.Value),
+			Name:     "dgps_u",
+			Value:    float64(f.Value) * 0.01,
+			RawValue: f.Value,
 		})
 		return signals
 	}),
-	mp.NewField("DGPS_v", 2, mp.Unsigned, mp.LittleEndian, func(f mp.Field) []mp.Signal {
+	mp.NewField("DGPS_v", 2, mp.Signed, mp.LittleEndian, func(f mp.Field) []mp.Signal {
 		signals := []mp.Signal{}
 		signals = append(signals, mp.Signal{
-			Name: "dgps_v", 
-			Value: int16(f.Value) * 0.01,
-			RawValue: int16(f.Value),
+			Name:     "dgps_v",
+			Value:    float64(f.Value) * 0.01,
+			RawValue: f.Value,
 		})
 		return signals
 	}),
-	mp.NewField("DGPS_w", 2, mp.Unsigned, mp.LittleEndian, func(f mp.Field) []mp.Signal {
+	mp.NewField("DGPS_w", 2, mp.Signed, mp.LittleEndian, func(f mp.Field) []mp.Signal {
 		signals := []mp.Signal{}
 		signals = append(signals, mp.Signal{
-			Name: "dgps_w", 
-			Value: int16(f.Value) * 0.01,
-			RawValue: int16(f.Value),
+			Name:     "dgps_w",
+			Value:    float64(f.Value) * 0.01,
+			RawValue: f.Value,
 		})
 		return signals
 	}),
