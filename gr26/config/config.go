acaa84f06712b15782708671f4fb5a4d2992b628
Andrey <andreyotvagingrey@gmail.com>
2026-05-26 00:43:21 -0700

    release: mapache v3.1.0

--- gr26/config/config.go
+++ gr26/config/config.go
@@ -8,7 +8,7 @@ import (
 
 var Service rincon.Service = rincon.Service{
 	Name:        "GR26",
-	Version:     "3.0.9",
+	Version:     "3.1.0",
 	Endpoint:    os.Getenv("SERVICE_ENDPOINT"),
 	HealthCheck: os.Getenv("SERVICE_HEALTH_CHECK"),
 }
