442bbea95d3a2aa0cd46b2ff6c9e1ee7d8115670
gr-jetson <team@gauchoracing.com>
2026-05-30 14:33:53 -0700

    release: mapache v3.1.5

--- auth/config/config.go
+++ auth/config/config.go
@@ -8,7 +8,7 @@ import (
 
 var Service rincon.Service = rincon.Service{
 	Name:        "Auth",
-	Version:     "3.1.4",
+	Version:     "3.1.5",
 	Endpoint:    os.Getenv("SERVICE_ENDPOINT"),
 	HealthCheck: os.Getenv("SERVICE_HEALTH_CHECK"),
 }
