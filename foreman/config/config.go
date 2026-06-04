fec395f26777dfe4deea76bd941946c5c35bcb89
Bharat Kathi <bharat1031@gmail.com>
2026-06-04 04:20:33 -0700

    release: mapache v3.4.1

--- foreman/config/config.go
+++ foreman/config/config.go
@@ -21,7 +21,7 @@ func (s ServiceInfo) PathPrefix() string {
 
 var Service = ServiceInfo{
 	Name:    "Foreman",
-	Version:     "3.4.0",
+	Version:     "3.4.1",
 }
 
 // Service-to-service auth was removed for fast iteration; all
