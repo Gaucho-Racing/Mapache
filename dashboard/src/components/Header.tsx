53e7bd2678fdcebffc9b7e0f10112d9ba780cd30
Bharat Kathi <bharat.kathi@withpersona.com>
2026-08-05 13:15:58 -0700

    fix: point sentinel references at sso.gauchoracing.com

--- dashboard/src/components/Header.tsx
+++ dashboard/src/components/Header.tsx
@@ -53,7 +53,7 @@ const Header = (props: HeaderProps) => {
               <DropdownMenuItem
                 onClick={() =>
                   window.open(
-                    "https://sentinel-v5.gauchoracing.com/settings",
+                    "https://sso.gauchoracing.com/settings",
                     "_blank",
                   )
                 }
